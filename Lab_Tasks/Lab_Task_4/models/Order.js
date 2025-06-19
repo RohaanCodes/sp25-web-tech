de49db0bdaa750109c0c2e49dd8677c04f73b22d
RohaanCodes <muhammadrohan042002@gmai.com>
2025-06-19 17:46:12 +0500

    lab task 04

---
+++ Lab_Tasks/Lab_Task_4/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      quantity: Number
+    }
+  ],
+  phone: String,
+  address: String,
+  payLater: Boolean,
+  deliveryDays: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
